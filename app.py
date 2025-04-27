662474cfab9db80fc4fff3232eb86319a395cf06
SANSKAR SHARMA <109541530+akashusharma@users.noreply.github.com>
2025-04-27 21:21:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from io import BytesIO
 
 st.set_page_config(page_title="Data Analysis", layout="wide")
 
-st.title("Data Analysis and Visualization Web Application")
+st.title("📊📈Data Analysis and Visualization Web Application")
 
 # Function to download plots
 def download_button(fig, filename):
