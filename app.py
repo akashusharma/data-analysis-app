799a9b34f2ed9b795d993e0976f2023622ddcbef
SANSKAR SHARMA <109541530+akashusharma@users.noreply.github.com>
2025-04-27 21:37:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from io import BytesIO
 st.set_page_config(page_title="Data Analysis", layout="wide")
 
 st.title("📈 Data Analysis and Visualization Web Application")
-st.subheader("Designed by Sanskar Sharma")
+st.subheader("   Designed by Sanskar Sharma")
+
 
 # Function to download plots
 def download_button(fig, filename):
